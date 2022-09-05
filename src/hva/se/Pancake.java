672404130261d84352dbd7f3b8356519411a2719
Michael Cheung <michael.cheung@hva.nl>
2022-09-05 12:49:46 +0200

    Feat: Created Pancake object

---
+++ src/hva/se/Pancake.java
@@ -0,0 +1,15 @@
+package hva.se;
+
+public class Pancake {
+
+    private final double diameter;
+
+
+    public Pancake(double diameter) {
+        this.diameter = diameter;
+    }
+
+    public double getDiameter() {
+        return diameter;
+    }
+}
