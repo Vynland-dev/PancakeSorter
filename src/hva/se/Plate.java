0036a19c2ee466ebbe3b925e4ef789afc87a8894
Michael Cheung <michael.cheung@hva.nl>
2022-09-09 10:28:41 +0200

    Feat: Plate now throws exception if attempting to add pancake of same value at end of the queue

--- src/hva/se/Plate.java
+++ src/hva/se/Plate.java
@@ -4,14 +4,24 @@ import java.util.ArrayDeque;
 
 public class Plate<E> extends ArrayDeque<Pancake> {
 
-    public void addFirst(Pancake pancake) throws IllegalArgumentException {
 
+
+    public void addFirst(Pancake pancake) throws IllegalArgumentException {
         if (this.stream().noneMatch(e -> e.getDiameter() == pancake.getDiameter())){
             super.addFirst(pancake);
         } else {
             throw new IllegalArgumentException("Element already exists");
         }
-
     }
+
+    public void addLast(Pancake pancake) throws IllegalArgumentException {
+        if (this.stream().noneMatch(e -> e.getDiameter() == pancake.getDiameter())){
+            super.addFirst(pancake);
+        } else {
+            throw new IllegalArgumentException("Element already exists");
+        }
+    }
+
+
 
 }
